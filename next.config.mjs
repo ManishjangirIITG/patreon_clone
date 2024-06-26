82480b703bcb9da240dcebc735d4d584384ebc88
ManishjangirIITG <m.jangir@iitg.ac.in>
2024-06-26 14:30:52 +0530

    made some changes in next.config.mjs for the build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const isExporting = process.env.NEXT_EXPORT === "true";
+
+export default {
+    // Disable API routes during 'next-export'
+    rewrites: async () => {
+        if (isExporting) {
+          return [
+            // Redirect API routes to a placeholder during export
+            {
+              source: '/api/:path*',
+              destination: '/api_disabled',
+            },
+          ];
+        }
+        // No rewrites needed during normal operation
+        return [];
+      },
+}
